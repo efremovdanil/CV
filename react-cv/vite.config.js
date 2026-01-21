540a1386c5ef095b089c9284083d90c3e957e8ab
efremovdanil <efremovdanil0@gmail.com>
2026-01-21 16:56:21 +0500

    Publish site to docs for GitHub Pages

--- react-cv/vite.config.js
+++ react-cv/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages: репозиторий `CV` деплоится по пути `/CV/`
+  base: '/CV/',
   plugins: [react()],
 })
